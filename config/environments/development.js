38aa5a3030657c3ac16e3fa86dc30feea951c017
Dmitry Kruglov <kruglov081289@mail.ru>
2021-05-23 14:36:33 +0300

    adds db service

--- config/environments/development.js
+++ config/environments/development.js
@@ -8,9 +8,4 @@ export default async (app) => {
 
   app.config.logLevel = 'debug';
   app.config.cacheModules = false;
-
-  app.config.db = {
-    type: 'sqljs',
-    location: 'db/development.sqlite',
-  };
 };
